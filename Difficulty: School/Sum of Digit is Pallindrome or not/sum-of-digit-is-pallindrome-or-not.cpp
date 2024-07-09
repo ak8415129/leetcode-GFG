c3c5cc34c3237c5577e124e704fb8278fcc44de9
Ankit Kumar <109846271+ak8415129@users.noreply.github.com>
2024-07-09 17:58:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: School/Sum of Digit is Pallindrome or not/sum-of-digit-is-pallindrome-or-not.cpp
+++ Difficulty: School/Sum of Digit is Pallindrome or not/sum-of-digit-is-pallindrome-or-not.cpp
@@ -6,7 +6,19 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
-
+bool help(string s ,int i ,int j)
+{
+    if(i==j)
+    return true;
+    
+    if(s[i]!=s[j])
+    return false;
+    
+   if(i<j+1)
+    return help(s,i+1,j-1);
+    
+    return true;
+}
 class Solution {
   public:
     int isDigitSumPalindrome(int n) {
@@ -20,18 +32,20 @@ class Solution {
         } 
         string st=to_string(sum);
         int i=0 ,j=st.size()-1;
-        while(i<=j)
-        {
-            if(st[i]!=st[j])
-            {
-                return 0;
-            } 
-            i++;
-            j--;
-        } 
-        return 1;
+        // while(i<=j)
+        // {
+        //     if(st[i]!=st[j])
+        //     {
+        //         return 0;
+        //     } 
+        //     i++;
+        //     j--;
+        // } 
+        // return 1; 
+        return help(st,i,j);
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
