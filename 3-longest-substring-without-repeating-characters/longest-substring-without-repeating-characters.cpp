c4c80bc9aaa7067a3bc07f0fffa92a7d7622d23e
Ankit Kumar <109846271+ak8415129@users.noreply.github.com>
2026-07-27 20:28:08 +0530

    Time: 10 ms (50.12%) | Memory: 12.4 MB (38.46%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -6,29 +6,46 @@ public:
         int high=0;
         int ans=0;
         unordered_map<char,int> mp;
-        while(high<n)
-        {  
-          
-
-            while(mp.find(s[high])!=mp.end())
+        for(high=0; high<n; high++)
+        {
+            mp[s[high]]++;
+            int k=(high-low)+1;
+            if(k==mp.size())
             {
-                mp[s[low]]--;
+                ans=max(ans,k);
+            } 
+            while(k>mp.size())
+            {
+                 mp[s[low]]--;
                 if(mp[s[low]]==0)
                 mp.erase(s[low]);
                 low++;
-            }
-              if(mp.find(s[high])==mp.end())
-            {
-              mp[s[high]]++;
-                int len=(high-low)+1;
-                ans=max(ans,len);
-
-            }
+                k=(high-low)+1; 
+            } 
+        }
+        // while(high<n)
+        // {  
           
-            high++;
+
+        //     while(mp.find(s[high])!=mp.end())
+        //     {
+        //         mp[s[low]]--;
+        //         if(mp[s[low]]==0)
+        //         mp.erase(s[low]);
+        //         low++;
+        //     }
+        //       if(mp.find(s[high])==mp.end())
+        //     {
+        //       mp[s[high]]++;
+        //         int len=(high-low)+1;
+        //         ans=max(ans,len);
+
+        //     }
+          
+        //     high++;
 
 
-        }  
+        // }  
 
         if(ans==INT_MAX)
         return 0;
