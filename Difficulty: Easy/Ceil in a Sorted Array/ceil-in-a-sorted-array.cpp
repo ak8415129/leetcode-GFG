00fdc7f1ed18e70b199b0f617dec12bd7d7d4674
Ankit Kumar <109846271+ak8415129@users.noreply.github.com>
2026-09-14 20:31:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Ceil in a Sorted Array/ceil-in-a-sorted-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    int findCeil(vector<int>& arr, int x) {
+        // code here 
+        int n=arr.size();
+        int low=0 ,high=n-1;
+        int ans=-1;
+        while(low<=high)
+        {
+           int mid=low+(high-low)/2;
+          
+           if(arr[mid]>=x)
+           { 
+               ans=mid;
+               high=mid-1;
+           }
+          else  if(arr[mid]<x)
+           {  
+               
+               
+               low=mid+1;
+           } 
+        }
+        
+        
+        
+        
+        return ans;
+        
+    }
+};
