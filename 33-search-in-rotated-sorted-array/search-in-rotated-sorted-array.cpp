e2b55a48564cd7142711db2bc10e0d850c321096
Ankit Kumar <109846271+ak8415129@users.noreply.github.com>
2026-09-15 20:21:45 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (37.45%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) { 
-         int n=nums.size();
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
         int low=0;
         int high=n-1;
         int res=0;
@@ -9,46 +9,38 @@ public:
         while(low<=high)
         { 
             int mid=low+(high-low)/2;
-
+            if(nums[mid]==target)
+            return mid;
+            // part 1 
             if(nums[mid]>nums[n-1])
-            low=mid+1;
-
-            else
             {
-               high=mid-1;
+                if(nums[mid]<target)
+                {
+                    low=mid+1;
+                } 
+                else{
+                    if(nums[0]>target)
+                    low=mid+1;
+                    else
+                    high=mid-1;
+                }
+            } 
+            // part 2
+            else{
+                if(nums[mid]>target)
+                {
+                    high=mid-1;
+                }
+                else{
+                    if(nums[n-1]<target)
+                    high=mid-1;
+                    else
+                    low=mid+1;
+                }
             }
+          
            
-        }  
-        int  rotate=low;
-         //  we have two part 
-         low=0 ,high=rotate-1;
-          while(low<=high)
-        {
-           int mid=low+(high-low)/2;
-            if(nums[mid]==target)
-            return mid;
-
-            else if(nums[mid]>target)
-            high=mid-1;
-            else
-            low=mid+1;
-        }
-
-         low=rotate ,high=n-1;
-         while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target)
-            return mid;
-
-            else if(nums[mid]>target)
-            high=mid-1;
-            else
-            low=mid+1;
-        }
-        
+        }   
         return -1;
-         
-        
     }
 };
